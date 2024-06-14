1df54727adfbf79e9a4056ff43cc07f8748785d9
erpccodes <erhimanshu924@gmail.com>
2024-06-14 16:06:00 +0530

    Initial Project setup

---
+++ backend/backend/src/main/java/com/example/backend/LmsApplication.java
@@ -0,0 +1,17 @@
+package com.example.backend;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class LmsApplication
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(LmsApplication.class, args);
+    }
+}
